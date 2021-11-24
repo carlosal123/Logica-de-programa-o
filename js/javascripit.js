e3619e58ce2d1a9d7abde5437a1d33d03680b03e
carlos_alberto_st@hotmail.com <carlos_alberto_st@hotmail.com>
2021-11-24 20:00:35 -0300

    first commit

--- js/javascripit.js
+++ js/javascripit.js
@@ -1,28 +1,101 @@
-let caixa =document.getElementById('caixa');
-//definir variavel caixa
+// let caixa =document.getElementById('caixa');
+// //definir variavel caixa
 
-// adicionar eventos na variavel caixa
-caixa.addEventListener('click', clicar);
-caixa.addEventListener('mouseenter', entrar);
-caixa.addEventListener('mouseout', sair);
+// // adicionar eventos na variavel caixa
+// caixa.addEventListener('click', clicar);
+// caixa.addEventListener('mouseenter', entrar);
+// caixa.addEventListener('mouseout', sair);
 
-// criar função da variavel clicar (ATIVA QUANDO O USUARIO CLICAR DENTRO DA DIV)
-function clicar(){
-    caixa.innerHTML ='você clicou';
-    caixa.style.background ='red';
-    caixa.style.color = 'white';
+// // criar função da variavel clicar (ATIVA QUANDO O USUARIO CLICAR DENTRO DA DIV)
+// function clicar(){
+//     caixa.innerHTML ='você clicou';
+//     caixa.style.background ='red';
+//     caixa.style.color ='white';
    
-}
-//criar função da variavel entrar (ATIVADA QUNADO O MOUSE PASSAR DENTRO DA DIV)
-function entrar(){
-    caixa.innerHTML = 'Entrou';
-    caixa.style.background = 'yellow';
-    caixa.style.color = 'black';
+// }
+// //criar função da variavel entrar (ATIVADA QUNADO O MOUSE PASSAR DENTRO DA DIV)
+// function entrar(){
+//     caixa.innerHTML = 'Entrou';
+//     caixa.style.background = 'yellow';
+//     caixa.style.color = 'black';
 
-}
-// Criar função da variavel sair(Ativa quando tirar o nome dnetro da div)
-function sair(){
-    caixa.innerHTML = '<strong>Passe o mouse</strong>';
-    caixa.style.background = 'blue';
-    caixa.style.color = 'porple';
-}
+// }
+// // Criar função da variavel sair(Ativa quando tirar o nome dnetro da div)
+// function sair(){
+//     caixa.innerHTML = '<strong>Passe o mouse</strong>';
+//     caixa.style.background = 'blue';
+//     caixa.style.color = 'porple';
+// }
+
+// Pegar a data atual do computador
+let agora = new Date();
+let diaSemana = agora.getDay();
+let data = document.getElementById('data');
+
+switch(diaSemana){
+ case 3:
+         console.log('quarta-feira');
+         break;       
+
+
+default:
+    console.log('Dia invalido');
+    
+break;
+
+
+ }
+
+//   if(diaSemana == 0){
+//         data.innerHTML =('Domingo');
+//         data.style.background = ('black');
+// } else if(diaSemana ==1){
+//         data.innerHTML = ('Segunda-feira');
+//         data.style.background = ('blue');
+   
+// } else if(diaSemana ==2){
+//         data.innerHTML = ('Terça-feira');
+//         data.style.background = ('red');
+
+// } else if(diaSemana ==3){
+//         data.innerHTML = ('Quarta-feira');
+//         data.style.background = ('green');
+
+// } else if(diaSemana ==4){
+//         data.innerHTML = ('Quinta-feira');
+//         data.style.background = ('black');
+
+// } else if(diaSemana ==5){
+//         data.innerHTML = ('Sexta-feira');
+//         data.style.background = ('yellow');
+
+// } else if(diaSemana ==6){
+//         data.innerHTML = ('Sábado');
+//         data.style.background = ('gray');
+
+// } else if(diaSemana ==7){
+//         data.innerHTML = ('Domingo');
+//         data.style.background = ('white');
+      
+        
+//     }
+
+    let contador = 0;
+
+    while(contador <= 300);
+    contador++;
+    console.log(contador);
+      
+        
+    
+      
+        
+    
+        
+    
+   
+     
+     
+        
+    
+ 
